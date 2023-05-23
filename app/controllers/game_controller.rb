0d2362063b6d0298cfc93509fa69807804eaa61c
Arthur Ligocki <artwligocki@gmail.com>
2023-05-23 11:32:44 -0300

    added better features

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,13 +8,14 @@ class GameController < ApplicationController
 
   def score
     start_time = params[:start_time].to_f
+
     @attempt = params[:word]
     response_hash = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{@attempt}").read)
+
     end_time = Time.now.to_f
     @elapsed_time = end_time - start_time
-    @score = ((@attempt.length * 10) / (@elapsed_time / 10)) if response_hash['found']
 
-    # @score = @attempt.length if response_hash['found']
+    @score = ((@attempt.length * 10) / (@elapsed_time / 10)) if response_hash['found']
     @score = 0 unless response_hash['found']
   end
 end
--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -1,18 +1,68 @@
 import { Controller } from "@hotwired/stimulus"
 
+let lastId;
+
 export default class extends Controller {
-  static targets = ["input"]
+  static targets = ["input", "button"]
+
   connect() {
-    console.log("connected");
+    lastId = 0
+  }
+
+
+  disableButton(button) {
+    button.disabled = true
+    button.classList.remove("btn-primary")
+    button.classList.add("btn-danger")
+    lastId += 1
+
+    button.setAttribute('data-id', lastId)
+  }
+
+  enableButton() {
+    this.buttonTargets.forEach((button) => {
+      if (parseInt(button.dataset.id) === lastId){
+        button.disabled = false
+        button.classList.add("btn-primary")
+        button.classList.remove("btn-danger")
+      }
+    })
+    lastId -= 1;
+  }
+
+  backspace(e){
+    e.preventDefault()
+    let newValue = this.inputTarget.value.split('')
+    newValue.splice(-1, 1)
+    newValue = newValue.join('')
+    this.inputTarget.value = newValue
+
+    this.enableButton()
+  }
+
+  keyCheck(e){
+    // console.log(e.key)
+    if (e.key === 'Backspace') {
+      this.backspace(e)
+    }
+
+    // console.log(this.buttonTargets)
+    for (let button of this.buttonTargets) {
+      // console.log(button)
+      if (button.value === e.key.toUpperCase()) {
+        if (!button.disabled) {
+          this.inputTarget.value += button.value
+          this.disableButton(button)
+          break
+        }
+      }
+    }
   }
 
   printInput(e){
     e.preventDefault()
-    e.currentTarget.disabled = true
-    e.currentTarget.classList.remove("btn-primary")
-    e.currentTarget.classList.add("btn-danger")
-
-    console.log(e.currentTarget.value)
+    this.disableButton(e.currentTarget)
     this.inputTarget.value += e.currentTarget.value
   }
+
 }
